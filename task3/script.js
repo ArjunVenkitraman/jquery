cb875b42bf81a62b5011c25728d04bb35e70c3e1
ArjunVenkitraman <ramukpully@gmail.com>
2023-03-15 08:24:18 +0530

    home

--- task3/script.js
+++ task3/script.js
@@ -2,34 +2,45 @@ function validate(){
 $(document).ready(function(){
     $('#form-detail').validate({
         rules:{
-            'uname':{
+            uname:{ 
                 required:true,
                 maxlength:12,
-                minlength:2
+                minlength:2,
+                pattern: /^[a-zA-Z]+$/
             },
-            'email':{
+            email:{
                 required:true,
                 email:true,
             },
-            'password':{
+            password:{
                 required:true,
-                
+                pattern:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$#])[A-Za-z\d@$#]{8,16}$/
             },
-            'mobile':{
+            mobile:{
                 required:true,
-                patternm:/^([0-9]{3}-[0-9]{3}-[0-9]{4})$/
-            }
+                pattern:/^[0-9]{3}-[0-9]{3}-[0-9]{4}$/
+            },
         },
         messages:{
-            'required':"Please enter a valid username with alphabets .",
-            'required':"This field is required.",
-            'email'   :"This field is required",
-            'email'   :"Email should follow abc@gmail.com format",
-            'required':"This field is required",
-            'patternp':"Password should contain numbers,alphabets,special characters etc",
-            'required':"This field is required",
-            'patternm':"Mobile number should follow 999-999-9999",
-        }
+            uname:{
+            required:"Please enter a valid username with alphabets .",
+            maxlength:"Username should not exeed 12 characters",
+            minlength:"Username should be atleast 2 characters",
+            pattern:"It should contain only alphabets"
+            },
+            email:{
+            required:"This field is required.",
+            email   :"Email should follow abc@gmail.com format",
+            },
+            password:{
+            required:"This field is required",
+            pattern:"Password should contain numbers,alphabets,special characters etc",
+            },
+            mobile:{
+            required:"This field is required",
+            pattern:"Mobile number should follow 999-999-9999",
+            },
+        },
     });
 });
 }
